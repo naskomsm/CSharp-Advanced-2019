795c0e1f301b6d07d81511ebe2fd33235ffea67d
Atanas Kolev <44707978+naskomsm@users.noreply.github.com>
2019-02-08 18:08:31 +0200

    Add files via upload

---
+++ DefiningClasses/Tupple/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Tupple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstInput = Console.ReadLine().Split();
+
+            string name = firstInput[0] + " " + firstInput[1];
+            string address = firstInput[2];
+
+            Tuppple<string, string> firstTuple = new Tuppple<string, string>(name,address);
+
+
+            string[] secondInput = Console.ReadLine().Split();
+
+            string secondName = secondInput[0];
+            int liters = int.Parse(secondInput[1]);
+
+            Tuppple<string, int> secondTuple = new Tuppple<string, int>(secondName, liters);
+
+            
+            string[] thirdInput = Console.ReadLine().Split();
+
+            int intNumber = int.Parse(thirdInput[0]);
+            double doubleNumber = double.Parse(thirdInput[1]);
+
+            Tuppple<int, double> thirdTuple = new Tuppple<int, double>(intNumber, doubleNumber);
+
+            Console.WriteLine(firstTuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirdTuple);
+
+        }
+    }
+}
